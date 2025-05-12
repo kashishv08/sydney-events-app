771024a011d079fb784e8a0229370da192a90eb7
kasahishv08 <kashish.vashistha1@gmail.com>
2025-05-12 23:43:42 +0530

    puppeteer changes

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 async function scrapeSydneyEvents() {
   const browser = await puppeteer.launch({ headless: true });
